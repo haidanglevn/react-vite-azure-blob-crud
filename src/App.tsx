ace6e2fd3d7f36d751b7c8c44f281d80fc7da96a
Dang Le <haidang.levn@gmail.com>
2024-01-22 16:36:35 +0200

    remove unused

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import "./App.css";
 import UploadPage from "./UploadPage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <UploadPage />
